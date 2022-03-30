92ecd2257a6142bd0fc32a0f8ff55f2a1448e92a
kmtrom <kylemtrom@gmail.com>
2022-03-29 19:57:09 -0500

    first commit

---
+++ src/main/java/com/crypto/cryptodash/CryptodashApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.cryptodash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptodashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptodashApplication.class, args);
+	}
+
+}
